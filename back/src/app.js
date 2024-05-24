19cef5448fb5462a7ae83a4aba28849a40a1d5ad
Favio10 <favio_olivera@hotmail.com>
2024-05-24 17:12:23 -0300

    .

---
+++ back/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+require ('./db')
+const server = express()
+server.name= 'API'
+
+server.use((err, req, res, next) => {
+    const status = err.status || 500
+    const message = err.message || err
+    console.log(err);
+    res.status(status).send(message)
+})
---
+++ back/src/db.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const {Sequelize} = require("sequelize")
+const {DB_USER, DB_PASSWORD, DB_HOST} = process.env
+
+const sequelize = new Sequelize(
+    `postgress://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/estacionamiento`,
+    {
+        logging: false,
+        native: false
+    }
+) 
