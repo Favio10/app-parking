b46adfeeb7e9a767e4b31f49e07d35b61e35beeb
Favio10 <favio_olivera@hotmail.com>
2024-05-24 17:41:00 -0300

    .

--- back/src/app.js
+++ back/src/app.js
@@ -1,12 +1,14 @@
-const express = require('express')
+const express = require("express");
 
-require ('./db')
-const server = express()
-server.name= 'API'
+require("./db");
+const server = express();
+server.name = "API";
 
 server.use((err, req, res, next) => {
-    const status = err.status || 500
-    const message = err.message || err
-    console.log(err);
-    res.status(status).send(message)
-})
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.log(err);
+  res.status(status).send(message);
+});
+
+module.exports = server;
--- back/src/db.js
+++ back/src/db.js
@@ -1,11 +1,15 @@
-require('dotenv').config()
-const {Sequelize} = require("sequelize")
-const {DB_USER, DB_PASSWORD, DB_HOST} = process.env
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-    `postgress://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/estacionamiento`,
-    {
-        logging: false,
-        native: false
-    }
-) 
+  `postgress://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/estacionamiento`,
+  {
+    logging: false,
+    native: false,
+  }
+);
+
+const basename = path.basename(__filename);
