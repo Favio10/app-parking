dd262e0003f36151d566a27c0c2d27be7fe69d27
Favio10 <favio_olivera@hotmail.com>
2024-05-26 20:12:01 -0300

    .

--- back/index.js
+++ back/index.js
@@ -5,6 +5,7 @@ const htpp = require("http");
 
 const app = htpp.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World");
 });
 
 const PORT = 3001;
