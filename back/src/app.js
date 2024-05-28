d3d65a0549a3e3f84c0bd981a5b3a11e133bd6b3
Favio10 <favio_olivera@hotmail.com>
2024-05-28 18:56:03 -0300

    .

--- back/src/app.js
+++ back/src/app.js
@@ -1,14 +1,15 @@
-// const express = require("express");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("./routes/index");
 
-// require("./db");
-// const server = express();
-// server.name = "API";
+require("./db");
 
-// server.use((err, req, res, next) => {
-//   const status = err.status || 500;
-//   const message = err.message || err;
-//   console.log(err);
-//   res.status(status).send(message);
-// });
+const server = express();
 
-// module.exports = server;
+server.name = "API";
+
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
+server.use(cookieParser());
