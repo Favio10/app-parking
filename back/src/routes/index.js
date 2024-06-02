cc79616a83dd3f3613216bfd003800f9c144f6dc
Favio10 <favio_olivera@hotmail.com>
2024-06-01 22:47:16 -0300

    .

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -2,11 +2,12 @@ const { Router } = require("express");
 
 const login = require("./login");
 const user = require("./user");
+const car = require("./car");
 
 const router = Router();
 
 router.use("/login", login);
 router.use("/user", user);
-router.use("/registerCar", regiCar);
+router.use("/registerCar", car);
 
 module.exports = router;
