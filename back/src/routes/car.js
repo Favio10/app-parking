ba8c681b5484a72f1b819da68703273957e527a4
Favio10 <favio_olivera@hotmail.com>
2024-06-08 18:45:29 -0300

    .

--- back/src/routes/car.js
+++ back/src/routes/car.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const {} = require("../controllers/registerCar");
+const { contRegisterOficial } = require("../controllers/registerCar");
 
 const carRoute = Router();
 
